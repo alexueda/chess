f7477e251820ada0b65c347a6d70617f65c2bd92
alexueda <alex402403@gmail.com>
2024-10-19 17:54:36 -0600

    LogoutService.java  is added

--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -1,4 +1,21 @@
 package service;
 
+import dataaccess.AuthDAO;
+import model.AuthData;
+
 public class LogoutService {
+    private final AuthDAO authDAO;
+
+    public LogoutService() {
+        this.authDAO = new AuthDAO();
+    }
+
+    public void logout(String authToken) {
+        AuthData authData = authDAO.getAuth(authToken);
+        if (authData == null) {
+            throw new IllegalArgumentException("Invalid auth token: Token does not exist.");
+        }
+
+        authDAO.deleteAuth(authToken);
+    }
 }
