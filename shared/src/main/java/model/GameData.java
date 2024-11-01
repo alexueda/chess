e5fd99b1c86b297c200add6806989a99010342f9
alexueda <alex402403@gmail.com>
2024-11-01 17:04:44 -0600

    little fix

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -2,8 +2,4 @@ package model;
 
 import chess.ChessGame;
 
-public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
-    public Object gameState() {
-        return null;
-    }
-}
+public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {}
