7064266d170ce1ad03a24d8b3ed9539d0091ec9d
alexueda <alex402403@gmail.com>
2024-11-10 23:19:29 -0700

    create the board try with matrix

--- client/src/main/java/ui/UIBoard.java
+++ client/src/main/java/ui/UIBoard.java
@@ -1,4 +1,34 @@
 package ui;
 
 public class UIBoard {
+    private String[][] board;
+
+    public UIBoard() {
+        this.board = new String[8][8];
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        board[0] = new String[]{"r", "n", "b", "q", "k", "b", "n", "r"};
+        board[1] = new String[]{"p", "p", "p", "p", "p", "p", "p", "p"};
+        for (int i = 2; i < 6; i++) {
+            for (int j = 0; j < 8; j++) {
+                board[i][j] = ".";
+            }
+        }
+        board[6] = new String[]{"P", "P", "P", "P", "P", "P", "P", "P"};
+        board[7] = new String[]{"R", "N", "B", "Q", "K", "B", "N", "R"};
+    }
+
+    public void printBoard() {
+        System.out.println("  a b c d e f g h");
+        for (int i = 0; i < 8; i++) {
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < 8; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println((8 - i));
+        }
+        System.out.println("  a b c d e f g h");
+    }
 }
