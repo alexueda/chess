3b1953a3f40940682c1cf16c0b2aae91996c5c1f
alexueda <alex402403@gmail.com>
2024-11-10 23:43:42 -0700

    upgrade my chess board design based on the requirement

---
+++ client/src/main/java/ui/TestUIBoard.java
@@ -0,0 +1,8 @@
+package ui;
+
+public class TestUIBoard {
+    public static void main(String[] args) {
+        UIBoard uiBoard = new UIBoard();
+        uiBoard.printBoard();  // This should print the chessboard to the terminal
+    }
+}
--- client/src/main/java/ui/UIBoard.java
+++ client/src/main/java/ui/UIBoard.java
@@ -1,34 +1,54 @@
 package ui;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class UIBoard {
-    private String[][] board;
+    private final String[][] board;
+    private static final String RESET = "\u001b[0m";
+    private static final String RED_TEXT = "\u001b[31m";
+    private static final String BLUE_TEXT = "\u001b[34m";
+    private static final String BG_LIGHT = "\u001b[47m";
+    private static final String BG_DARK = "\u001b[100m";
 
     public UIBoard() {
-        this.board = new String[8][8];
+        board = new String[8][8];
         initializeBoard();
     }
 
     private void initializeBoard() {
-        board[0] = new String[]{"r", "n", "b", "q", "k", "b", "n", "r"};
-        board[1] = new String[]{"p", "p", "p", "p", "p", "p", "p", "p"};
+        board[0] = new String[]{"R", "N", "B", "Q", "K", "B", "N", "R"};
+        board[1] = new String[]{"P", "P", "P", "P", "P", "P", "P", "P"};
         for (int i = 2; i < 6; i++) {
             for (int j = 0; j < 8; j++) {
-                board[i][j] = ".";
+                board[i][j] = " ";
             }
         }
-        board[6] = new String[]{"P", "P", "P", "P", "P", "P", "P", "P"};
-        board[7] = new String[]{"R", "N", "B", "Q", "K", "B", "N", "R"};
+        board[7] = new String[]{"r", "n", "b", "q", "k", "b", "n", "r"};
+        board[6] = new String[]{"p", "p", "p", "p", "p", "p", "p", "p"};
     }
 
     public void printBoard() {
-        System.out.println("  a b c d e f g h");
-        for (int i = 0; i < 8; i++) {
-            System.out.print((8 - i) + " ");
-            for (int j = 0; j < 8; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println((8 - i));
+        PrintStream out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        out.print("   ");
+        for (char col = 'h'; col >= 'a'; col--) {
+            out.print(" " + col + " ");
         }
-        System.out.println("  a b c d e f g h");
+        out.println();
+        for (int i = 0; i < 8; i++) {
+            out.print((8 - i) + " ");
+            for (int j = 7; j >= 0; j--) {
+                boolean isLightSquare = (i + j) % 2 == 0;
+                String bgColor = isLightSquare ? BG_LIGHT : BG_DARK;
+                String textColor = board[i][j].equals(board[i][j].toLowerCase()) ? BLUE_TEXT : RED_TEXT;
+                out.print(bgColor + " " + textColor + board[i][j] + " " + RESET);
+            }
+            out.println(" " + (8 - i));
+        }
+        out.print("   ");
+        for (char col = 'h'; col >= 'a'; col--) {
+            out.print(" " + col + " ");
+        }
+        out.println();
     }
 }
